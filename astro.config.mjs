b4071ba4df885f3d1921afb6710c3cb38d1f28b7
felix <info@blackspike.com>
2024-09-21 07:18:53 +0100

    added geo server island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     serverIslands: true,
   },
+
+  output: 'server',
+  adapter: cloudflare(),
 });
